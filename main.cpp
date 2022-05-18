312a360bbf8ed88d2175b8aa9fbed0e936ff04d1
YunYun <sakuralove72@mail.ru>
2022-05-18 22:17:33 +0300

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-#include <sdt.h>
+#include <C:\Users\Юлия\Desktop\lab-8\sdt.h>
+#include <stdio.h>
 main()
 {
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("\nEnter the second number: ");
+    scanf("%d", &b);
+    return 0;
 }
